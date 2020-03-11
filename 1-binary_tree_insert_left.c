280027eb15dea29db2e67faa13e336cbc41e85b0
Daniel Quintero <dgquintero02@hotmail.com>
2020-03-11 17:06:36 -0500

    adding descrotion to variables

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
  * binary_tree_insert_left - function that inserts a node as the left-child
- * parent : is a pointer to the node to insert the left-child in
- * value : the value to put in the new node
+ * @parent : is a pointer to the node to insert the left-child in
+ * @value : the value to put in the new node
  * Return: a pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
