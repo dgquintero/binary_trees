1698b551d5dc47b9294528b6866e184892526007
Daniel Quintero <dgquintero02@hotmail.com>
2020-03-11 16:08:53 -0500

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,17 +7,17 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-    new_node->parent = parent;
-    new_node->n = value;
-    new_node->right = NULL;
-    new_node->left = NULL;
-    return (new_node);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->right = NULL;
+	new_node->left = NULL;
+	return (new_node);
 }
