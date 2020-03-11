5733e6c255a9572e6ff3dafb296181fb850fb120
Daniel Quintero <dgquintero02@hotmail.com>
2020-03-11 17:04:19 -0500

    project update

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
  * binary_tree_insert_right - inserts a node as the right-child of another node
- * parent: is a pointer to the node to insert the right-child in
- * value: the value to put in the new node
+ * parent : is a pointer to the node to insert the right-child in
+ * value : the value to put in the new node
  * Return: a pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_delete - function that deletes an entire binary tree
- * tree: is a pointer to the root node of the tree to delete
+ * tree : is a pointer to the root node of the tree to delete
  * Return: Nothing
  */
 
