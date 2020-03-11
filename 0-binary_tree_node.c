3847837b4a0d764174e73140f4543283ce9e9766
Daniel Quintero <dgquintero02@hotmail.com>
2020-03-11 16:03:01 -0500

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
         free(new_node);
         return (NULL);
     }
-
     new_node->parent = parent;
     new_node->n = value;
     new_node->right = NULL;
