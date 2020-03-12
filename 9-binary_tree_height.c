09bce09287807e48569de4359f0c8c561de58e11
Daniel Quintero <dgquintero02@hotmail.com>
2020-03-12 11:41:31 -0500

    project update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,16 +6,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	size_t left_height = 0, right_height = 0;
 
-    if (tree == NULL)
-        return (0);
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-    if (left_height >= right_height)
-    {
-        return (left_height + 1);
-    else
-        return (right_height +1);
-    }
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
 }
