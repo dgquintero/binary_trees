0f61011bd950e6ec2e4d446fe1be44d38aa30c36
Daniel Quintero <dgquintero02@hotmail.com>
2020-03-13 15:56:08 -0500

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,10 +7,10 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent->parent)
-		return (NULL);
+		return (0);
 	if (node->parent == node->parent->parent->left)
-        return (node->parent->parent->right);
-    if (node->parent == node->parent->parent->right)
-        return (node->parent->parent->left);
-    return (NULL);
+		return (node->parent->parent->right);
+	if (node->parent == node->parent->parent->right)
+		return (node->parent->parent->left);
+	return (0);
 }
