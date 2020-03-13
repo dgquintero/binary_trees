614a292be53cc3d8b44d373320d04e372ebc5bd6
Daniel Quintero <dgquintero02@hotmail.com>
2020-03-13 12:37:29 -0500

    project update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,17 +4,17 @@
  * @tree : is a pointer to the root
  * Return: 1 if is full 0 if is not.
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t left_full = 0, right_full = 0;
 
 	if (tree == NULL)
 		return (0);
 	if (tree->left)
-		left_full += binary_tree_is_full(tree->left);
+		left_full += binary_tree_is_perfect(tree->left);
 	if (tree->right)
-		right_full += binary_tree_is_full(tree->right);
-    if ((left_full + right_full) % 2 == 0)
-        return (1);
-    return (0);
+		right_full += binary_tree_is_perfect(tree->right);
+	if ((left_full + right_full) % 2 == 0)
+		return (1);
+	return (0);
 }
