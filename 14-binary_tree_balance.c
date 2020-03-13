9896d00244eddd8bf22010fd5d18b82c8e02205a
Daniel Quintero <dgquintero02@hotmail.com>
2020-03-13 12:13:39 -0500

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,7 +26,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0, balance = 0;
-	
+
 	if (tree == NULL)
 		return (0);
 	if (tree->left)
