7225a94dfbeb9d99efe7030719ce942a21d4dc50
Daniel Quintero <dgquintero02@hotmail.com>
2020-03-13 14:30:33 -0500

    16 update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,13 +25,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_full = 0, right_full = 0;
-
 	if (tree == NULL)
 		return (0);
-	left_full = binary_tree_height(tree->left) + 1;
-	right_full = binary_tree_height(tree->right) + 1;
-	if (left_full == right_full)
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 		return (1);
 	return (0);
 }
